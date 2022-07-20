bc9a42586c8d52826990980c9b9b68d84eff8fa7
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2022-07-20 18:14:52 +0530

    Add Initial Directory Setup

---
+++ src/Components/App.js
@@ -0,0 +1,8 @@
+import EditTwoToneIcon from '@mui/icons-material/EditTwoTone';
+
+function App() {
+  return (
+      <div>Hello World <EditTwoToneIcon></EditTwoToneIcon></div>
+  );
+}
+export default App;
